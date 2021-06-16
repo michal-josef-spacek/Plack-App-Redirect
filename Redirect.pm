29cdb4274a284fa583aa5e1566f7edad430db408
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-06-16 17:49:12 +0200

    Prepare for 0.02 version

--- Redirect.pm
+++ Redirect.pm
@@ -8,7 +8,7 @@ use Plack::Response;
 use Plack::Request;
 use Plack::Util::Accessor qw(redirect_url);
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 sub call {
 	my ($self, $env) = @_;
@@ -168,6 +168,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
